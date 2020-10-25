035926aac8e24a6dcfb28730fc2a0e19dc9a067d
cuishengxian <15631942662@163.com>
2020-10-25 08:21:16 +0800

    Signed-off-by: cuishengxian <15631942662@163.com>

---
+++ build/precache-manifest.57b511c7cf923b0c18fef95dd7b217f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198db5d062b0573d48767f04345391cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b607e1e91877dd7714d",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "f73e23e9e37f6fe890f0",
+    "url": "/static/css/main.f77a28a4.chunk.css"
+  },
+  {
+    "revision": "9b607e1e91877dd7714d",
+    "url": "/static/js/2.6357decf.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.6357decf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73e23e9e37f6fe890f0",
+    "url": "/static/js/main.4765e33c.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
